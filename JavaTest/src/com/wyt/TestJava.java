c2a5499fc74e6a4c5a95883c3591894854865fd4
wanyongtao1988 <wanyongtao@wanyongtao-PC>
2014-02-24 10:18:50 +0800

    test

--- JavaTest/src/com/wyt/TestJava.java
+++ JavaTest/src/com/wyt/TestJava.java
@@ -4,7 +4,9 @@ public class TestJava {
 
 	public static void main(String[] args) {
 		System.out.println("hello github");
-
+		System.out.println("hello world");
+		System.out.println("hello world");
+		System.out.println();
 	}
 
 }
